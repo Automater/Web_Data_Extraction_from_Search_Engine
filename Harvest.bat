e3d7fb2da558718c159df02addbe8e21f1d2b219
Automater <patrickong@gmail.com>
2013-07-27 08:17:28 +0800

    Extract search data from the Web and save it to a database

---
+++ Harvest.bat
@@ -0,0 +1,3 @@
+cd "C:\Program Files (x86)\Automation Anywhere 7.0\"
+@ECHO Run application 
+"Automation Anywhere.exe" "/fC:\Users\Photoshop\Documents\Automation Anywhere Files\Automation Anywhere\My Tasks\Data_Harvesting.atmx/e"
