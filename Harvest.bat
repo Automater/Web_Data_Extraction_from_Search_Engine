c9daab0178bee05b74b1d223c05c55683c99d36e
Patrick Ong <patrickong@gmail.com>
2013-07-30 19:03:44 +0800

    Update Harvest.bat

--- Harvest.bat
+++ Harvest.bat
@@ -1,3 +1,6 @@
-cd "C:\Program Files (x86)\Automation Anywhere 7.0\"
+::  You need to download the trial "Automation Anywhere Server 7.0", install and copy everything in the "Client" folder into "Automation Anywhere 7.0" folder before you can run this batch file successfully.
+
+
+cd "C:\Program Files (x86)\Automation Anywhere 7.0\Client\"
 @ECHO Run application 
-"Automation Anywhere.exe" "/fC:\Users\Photoshop\Documents\Automation Anywhere Files\Automation Anywhere\My Tasks\Data_Harvesting.atmx/e"
+"AAPlayer.exe" "/fC:\Users\Photoshop\Documents\Automation Anywhere Files\Automation Anywhere\My Tasks\Data_Harvesting.atmx/e"
